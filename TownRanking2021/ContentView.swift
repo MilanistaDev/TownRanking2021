d43f6e1e49c0ac2fcb3f32ea7917622d4be6d636
Takuya Aso <milanista224@gmail.com>
2021-03-06 22:47:03 +0900

    Initial Commit

---
+++ TownRanking2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TownRanking2021
+//
+//  Created by Takuya Aso on 2021/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TownRanking2021/TownRanking2021App.swift
@@ -0,0 +1,17 @@
+//
+//  TownRanking2021App.swift
+//  TownRanking2021
+//
+//  Created by Takuya Aso on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct TownRanking2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
