0ca4622ea4ff9ecf05e1c74c9d7804e743c4011c
Takuya Aso <milanista224@gmail.com>
2021-03-07 23:17:49 +0900

    Added Extension to make Color Assets easier to use in SwiftUI.

---
+++ TownRanking2021/Extension/Color+SwiftUI.swift
@@ -0,0 +1,17 @@
+//
+//  Color+SwiftUI.swift
+//  TownRanking2021
+//
+//  Created by Takuya Aso on 2021/03/07.
+//
+
+import SwiftUI
+
+extension Color {
+    static let rentOrange = Color("rentOrange")
+    static let buyBlue = Color("buyBlue")
+    static let subRentOrange = Color("subRentOrange")
+    static let subBuyBlue = Color("subBuyBlue")
+    static let gridBackground = Color("gridBackground")
+    static let rowBackground = Color("rowBackground")
+}
